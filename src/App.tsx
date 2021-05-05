04e9dbf3de363154fc7a251cfc10d5d6a2f2ea63
S.Takemoto <akatsukisatoshi@gmail.com>
2021-05-05 17:27:01 +0900

    todoリストをts対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+type Todo = {
+  id: number,
+  task: string,
+  checked: boolean
+};
+
+type Todos = Todo[];
 
 function App() {
+  const [idCounter, setIdCounter] = useState(0);
+  const [todos, setTodo] = useState<Todos>([]);
+
+  /**
+   * フォーム送信したらtodo配列にtodoを追加
+   * @param {EventReact.FormEvent<HTMLFormElement>} e 送信イベント
+   */
+  const handleSubmit = (e :React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const inputText = e.currentTarget["task"].value;
+    const nextid = idCounter + 1;
+    setIdCounter(nextid);
+    setTodo([...todos, { id: nextid, task: inputText, checked: false }]);
+  };
+
+  /**
+   * 指定idをtodo配列から取り除く
+   * @param {number} id
+   */
+  const handleClickDeleteButton = (id :number) => {
+    setTodo(todos.filter((todo: Todo) => todo.id !== id));
+  };
+
+  /**
+   * TODOのチェックボックスがクリックされたら該当の checked フラグを toggle する
+   * @param {number} id
+   */
+  const handleChangeCheckBox = (id: number) => {
+    const changedTodos = todos.map((todo: Todo) => {
+      if (todo.id === id) {
+        return { ...todo, checked: !todo.checked };
+      }
+      return todo;
+    });
+    setTodo(changedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input name="task" />
+        <button>登録</button>
+      </form>
+      <div>
+        {todos.map((todo: Todo) => (
+          <div key={todo.id} className={todo.checked ? "checked" : ""}>
+            <input
+              type="checkbox"
+              onChange={() => handleChangeCheckBox(todo.id)}
+            />
+            {todo.task}
+            <button onClick={() => handleClickDeleteButton(todo.id)}>
+              削除
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
