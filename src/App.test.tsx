6b51c62e7298eec9e4fbc069c59eaf43421a4d22
S.Takemoto <akatsukisatoshi@gmail.com>
2021-05-05 19:43:15 +0900

    初期画面のテストを書いた

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('初期画面に投稿フォームが含まれている', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('登録')).toBeInTheDocument();
 });
